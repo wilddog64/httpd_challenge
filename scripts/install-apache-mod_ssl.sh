d1e47aab7d75b1792f8b5348531afe95d7523242
chengkai liang <chengkli@cisco.com>
2023-01-12 17:40:50 -0800

    remove debug flags

--- scripts/install-apache-mod_ssl.sh
+++ scripts/install-apache-mod_ssl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -vx
+#!/bin/bash
 # ################################################ #
 # install-apache-mod_ssl                           #
 # install and configure apache along with SSL      #
